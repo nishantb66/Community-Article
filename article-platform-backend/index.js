3004617d3e88c0bc2e9274003da08e8fb88c4720
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-02 23:07:20 +0530

    Update index.js

--- article-platform-backend/index.js
+++ article-platform-backend/index.js
@@ -8,8 +8,16 @@ const emailRoutes = require("./routes/email");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+const corsOptions = {
+  origin: 'https://community-article-frontend.vercel.app', // Allow requests from this frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you use cookies or authentication
+};
+
 // Middleware
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json()); // Middleware to parse JSON
 app.use("/api", emailRoutes); // Register the email route
