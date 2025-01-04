b515c25d37997d66a7c183bd5a8f9365580d028a
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-05 00:12:54 +0530

    Update page.js

--- article-platform-frontend/src/app/page.js
+++ article-platform-frontend/src/app/page.js
@@ -21,6 +21,13 @@ export default function Home() {
   const [collaborateReason, setCollaborateReason] = useState("");
   const [collaborateMessage, setCollaborateMessage] = useState("");
 
+  useEffect(() => {
+   var _mtm = window._mtm = window._mtm || [];
+   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.async=true; g.src='https://cdn.matomo.cloud/simplearticlesspace.matomo.cloud/container_3s7vGxHg.js'; s.parentNode.insertBefore(g,s);
+  }, [])
+
   // Fetch articles from the backend API
   useEffect(() => {
     fetch("https://community-article-backend.onrender.com/api/articles")
