72d0abdd3e05aed4d05d8a24301500094ad90523
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-09 00:04:47 +0530

    Update User.js

--- article-platform-backend/models/User.js
+++ article-platform-backend/models/User.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false }, // New field to track verification status
   interestedDomains: { type: [String], default: [] },
-  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Article" }], // Reference to the Article model
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Article" }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
+
