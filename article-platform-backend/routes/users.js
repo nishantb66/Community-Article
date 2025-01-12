c27ea5886d19696ea5de497ec801f8febd53b0b9
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-12 19:47:11 +0530

    Update users.js

--- article-platform-backend/routes/users.js
+++ article-platform-backend/routes/users.js
@@ -196,4 +196,25 @@ router.post("/bookmarks/remove", async (req, res) => {
   }
 });
 
+// Get User Details by ID
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: "Invalid userId format" });
+  }
+
+  try {
+    const user = await User.findById(userId).select("-bookmarks"); // Include password, exclude bookmarks
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
